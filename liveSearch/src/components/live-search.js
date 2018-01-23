8b2b11df0f45a434efef46a9fe0c430a8e1587b2
John Castillo <JohnluisCastillo@gmail.com>
2018-01-23 15:43:57 -0500

    <3

--- liveSearch/src/components/live-search.js
+++ liveSearch/src/components/live-search.js
@@ -4,12 +4,43 @@ import SearchForm from './search-form';
 import CharacterCount from './character-count';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
-    return ( 
-        <div className="live-search">
-            <SearchForm />
-            <CharacterCount count={props.characters.length} />
-            <CharacterList characters={props.characters} />
-        </div>
-    );
+export default class LiveSearch extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            searchVal: '',
+            characters: [{
+                name: 'drake',
+                actor: 'The Rock',
+                description: 'whats up'
+            },
+            {
+                name: 'static',
+                actor: 'Shock',
+                description: 'tin man'
+            }]
+        };
+    }
+    filteredCharacters() {
+        if(this.state.searchVal === ''){
+            return this.state.characters;
+        }
+        const filtered = this.state.characters.filter(chr => { 
+            return chr.name.toLowerCase().includes(this.state.searchVal.toLowerCase());
+        });
+
+
+        return filtered;
+    }
+    render(){
+        console.log(this.state.searchVal);
+        return ( 
+            <div className="live-search">
+                <SearchForm onChange={search => this.setState({searchVal:search})} />
+                <CharacterCount count={this.filteredCharacters().length} />
+                <CharacterList characters={this.filteredCharacters()} />
+            </div>
+        );
+
+    }
 }
